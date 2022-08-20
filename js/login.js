b111c99720503ccaa92f85802c7205723753ca00
brianmachado <brian.machado2180@gmail.com>
2022-08-20 19:26:14 -0300

    ok

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(e){
 enviar.addEventListener("click", (e) => {
     let inputs = [floatingInput.value, floatingPassword.value];
     if (inputs.every(notEmpty)){
-        window.location.href="index.html";
+        window.location.href="indexx.html";
     } else {
         alert("Complete los datos");
     }
