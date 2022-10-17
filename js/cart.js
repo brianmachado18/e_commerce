c95833ef3571a71656efd15379c73c51f1d3beb3
brianmachado18 <110497734+brianmachado18@users.noreply.github.com>
2022-10-16 21:03:14 -0300

    y

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,37 @@
+const USER_URL = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
+let articlesArray = [];
+
 document.addEventListener('DOMContentLoaded', ()=>{
     //entrega 2
     email.innerHTML = `${localStorage.getItem('user')}`;
+    fetch(USER_URL)
+    .then(response => response.json())
+    .then(data =>{
+        articlesArray = data.articles;
+        showArticlesArray();
+    });
 });
+
+function showArticlesArray(){
+    for (let i = 0; i < articlesArray.length; i++){
+        let htmlContentToAppend = 
+        `<tr><td style="width: 275px;"><img src="${articlesArray[i].image}" style="min-width: 100px;" alt="${articlesArray[i].name}" class="img-thumbnail img-fluid"></td>
+        <td>${articlesArray[i].name}</td>
+        <td>${articlesArray[i].currency} ${articlesArray[i].unitCost}</td>
+        <td><input type="number" id="${articlesArray[i].id}" min="1" max="99" value="${articlesArray[i].count}"></td>
+        <td class="fw-bold" id="${articlesArray[i].id}-cost"></td></tr>` 
+        
+        articles.innerHTML = htmlContentToAppend;
+        
+        let userInput = document.getElementById(`${articlesArray[i].id}`);
+        let cost =  articlesArray[i].unitCost;
+        let newCost = document.getElementById(`${articlesArray[i].id}-cost`);
+        
+        newCost.innerHTML = `${articlesArray[i].currency} ${userInput.value * cost}`;
+        userInput.addEventListener("input", function(){ // This event uploads our subtotal in real time when the quantity input gets changed
+            newCost.innerHTML = `${articlesArray[i].currency} ${userInput.value * cost}`;
+            articlesArray[i].count = userInput.value;
+            localStorage.setItem("cartArray", JSON.stringify(articlesArray));
+        });
+    }   
+};
